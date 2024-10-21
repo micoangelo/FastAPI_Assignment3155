eeb135212bc08f212c3a9b83309c3aa33d78c844
Mico <mico.masangkay02@gmail.com>
2024-10-20 22:23:34 -0400

    feat: create initial FastAPI setup with GET and PUT requests

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    price: float
+    description: str = None
+    tax: float = None
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello World"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_id": item_id, "item": item}
+
